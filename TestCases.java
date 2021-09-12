f150ad5ae280af1bdf25c4356db8cdd65e5ba2e6
adumasiv <81191929+adumasiv@users.noreply.github.com>
2021-09-11 21:56:18 -0400

    Add files via upload

---
+++ TestCases.java
@@ -0,0 +1,61 @@
+/*
+
+What is proposition p? true
+
+What is proposition q? true
+
+The conjunction truth value is: true
+The disjunction truth value is: true
+The exclusive OR truth value is: false
+The conditional truth value is: true
+The biconditional truth value is: true
+
+
+What is proposition p? true
+
+What is proposition q? false
+
+The conjunction truth value is: false
+The disjunction truth value is: true
+The exclusive OR truth value is: true
+The conditional truth value is: false
+The biconditional truth value is: false
+
+
+What is proposition p? false
+
+What is proposition q? true
+
+The conjunction truth value is: false
+The disjunction truth value is: true
+The exclusive OR truth value is: true
+The conditional truth value is: true
+The biconditional truth value is: false
+
+
+What is proposition p? false
+
+What is proposition q? false
+
+The conjunction truth value is: false
+The disjunction truth value is: false
+The exclusive OR truth value is: false
+The conditional truth value is: true
+The biconditional truth value is: true
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
